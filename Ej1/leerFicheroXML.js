9788f369a960a5c6bde2d875d7416c718728294e
AinaraSerna01 <76426392+AinaraSerna01@users.noreply.github.com>
2021-05-14 09:46:45 +0200

    Update leerFicheroXML.js

--- Ej1/leerFicheroXML.js
+++ Ej1/leerFicheroXML.js
@@ -1,4 +1,10 @@
 function gestionarFicheroXML(xmlDoc){
 	alert(xmlDoc)
+	let capaVacia = document.querySelector("#ficheroXML")
+	let libros = xmlDoc.querySelectorAll("libros")
+	for(let i=0; i<libros.length; i++){
+		capaVacia.innerHTML += "<p>" + libros[i].textContent + "</p>"
+	}
+	
 }
 loadDocA("libros.xml","xml") /*loadDocA --> para llamar un fichero y su tipo*/
