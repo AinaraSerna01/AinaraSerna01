ef7651902b7ff6e51652049bc6f31cf950a53a02
AinaraSerna01 <76426392+AinaraSerna01@users.noreply.github.com>
2021-05-14 09:20:58 +0200

    Update leerFicheroXML.js

--- Ej1/leerFicheroXML.js
+++ Ej1/leerFicheroXML.js
@@ -6,9 +6,8 @@ function gestionarFicheroXML(xmlDoc){
 	
 }
 
-let capa = document.querySelector("div:nth-child(1)") 
+let capa = document.querySelector("div:nth-child(1)")
 capa.addEventListener("click",CargarFichero);
-function CargarFichero()
-{
-	loadDocA("leerFicheroXML.xml","xml");
+function CargarFichero(){
+	loadDocA("leerFicheroXML.xml","xml"); /*loadDocA --> para llamar un fichero y su tipo*/
 }
