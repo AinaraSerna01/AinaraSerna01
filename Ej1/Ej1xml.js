eba88c34e8520b0c2ee6c2fdba50ed9e13174a6a
AinaraSerna01 <76426392+AinaraSerna01@users.noreply.github.com>
2021-05-20 10:17:06 +0200

    Update Ej1xml.js

--- Ej1/Ej1xml.js
+++ Ej1/Ej1xml.js
@@ -1,13 +1,11 @@
 loadLDocA("libros.xml","xml")
 function gestionarFicheroXML(xmltree){
-
     let capa = document.querySelector(".árbolXML")
-let libros = xmltree.querySelector("libro")
-for(let i=0; i<alumnos.length; i++){
-    capa.innerHTML += capa.innerHTML + "<p>" + libros[i].textContent + "</p>"
-}
-
-let libro3 = xmltree.querySelector("libro:nth-clild(3)")
-alert("Cambio del 3º libro")
-libro3[0].className="cambio"
+    let libros = xmltree.querySelector("libro")
+    for(let i=0; i<alumnos.length; i++){
+      capa.innerHTML += capa.innerHTML + "<p>" + libros[i].textContent + "</p>"
+    }
+    let libro3 = xmltree.querySelector("libro:nth-clild(3)")
+    alert("Cambio del 3º libro")
+    libro3[0].className="cambio"
 }
