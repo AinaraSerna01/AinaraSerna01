2c379fdbc835dea64976999a0748039dd05ea6f7
AinaraSerna01 <76426392+AinaraSerna01@users.noreply.github.com>
2021-05-20 11:22:37 +0200

    Update Ej2.js

--- Ej2/Ej2.js
+++ Ej2/Ej2.js
@@ -1,9 +1,17 @@
-function gestionarTXT(txtlist){
-    let lineas = txtlist.split("-")
-    let item = document.querySelector(".listaTXT")
-    for(let i of lineas){
-        item.innerHTML += + "<p>" + i + "</p>"
+loadLDocA("Ej2.xml","xml")
+function gestionarFicheroXML(xmltree){
+    let capa = document.querySelector(".arbolXML")
+    let mensajes = xmltree.querySelectorAll("mensaje")
+    
+    console.log(mensajes.length)
+    for(let i=0; i<mensajes.length; i++){
+        console.log("Entro al bucle")
+        if(i == 1 || i== 3 || i==5){
+            capa.innerHTML += "<p class='impar'>" + mensajes[i].textContent + "</p>"
+            
+        } else {
+            capa.innerHTML += "<p class='par'>" + mensajes[i].textContent + "</p>"
+        }
     }
+  
 }
-
-let txtlist = LoadLDocA("Ej2.txt", "txt")
