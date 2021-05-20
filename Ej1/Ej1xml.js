ee343779814906399fe01a80bc6b8d33329d3e3f
carmenFRP <33463902+carmenFRP@users.noreply.github.com>
2021-05-20 10:24:48 +0200

    Update Ej1xml.js

--- Ej1/Ej1xml.js
+++ Ej1/Ej1xml.js
@@ -1,7 +1,8 @@
 loadLDocA("libros.xml","xml")
 function gestionarFicheroXML(xmltree){
-    let capa = document.querySelector(".árbolXML")
+    let capa = document.querySelector("arbolXML")
     let libros = xmltree.querySelector("libro")
+    console.log(libros)
     for(let i=0; i<libros.length; i++){
       capa.innerHTML += capa.innerHTML + "<p>" + libros[i].textContent + "</p>"
     }
