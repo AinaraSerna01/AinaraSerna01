db9438d5cc0344a60ddca69b4d5b69c8a0ce821a
AinaraSerna01 <76426392+AinaraSerna01@users.noreply.github.com>
2021-05-21 14:09:59 +0200

    Update Ej2.js

--- Ej2/Ej2.js
+++ Ej2/Ej2.js
@@ -6,10 +6,11 @@ function gestionarFicheroXML(xmltree){
     console.log(mensajes.length)
     for(let i=0; i<mensajes.length; i++){
         console.log("Entro al bucle")
-        if(i == 0){
+        let res = i%2
+        if(res==0){
             capa.innerHTML += "<p class='par'>" + mensajes[i].textContent + "</p>"
         } else {
-            capa.innerHTML += "<p>" + mensajes[i].textContent + "</p>"
+            capa.innerHTML += "<p class='impar'>" + mensajes[i].textContent + "</p>"
         }
     } 
 }
