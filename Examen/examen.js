c79206b7b656081a76a1d02eb00448dd2cc38cb7
AinaraSerna01 <76426392+AinaraSerna01@users.noreply.github.com>
2021-05-24 09:15:34 +0200

    Add files via upload

---
+++ Examen/examen.js
@@ -0,0 +1,22 @@
+LoadLDocA("examen.xml","xml")
+function gestionarFicheroXML(xmltree){
+    let capa = document.querySelector(".arbolXML")
+    let animales = xmltree.querySelectorAll("animal")
+
+    console.log(animales.length)
+    for(let i=0; i<animales.length; i++){
+        capa.innerHTML += "<p>" + animales[i].textContent + "</p>"
+    }
+}
+
+addEventListener("mouseover", function(){
+    alert("Cambio de estilo")
+    for(let i=0; i<animales.length; i++){
+        let res = i % 2
+        if (res == 0){
+            animales[i].className="par"
+        } else {
+            animales[i].className="impar"
+        }
+    }
+})
